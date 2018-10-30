e6030e1cdc11012f91f5a696cd04d3641faf6fdc
samanthalee <samtomlee@gmail.com>
2018-10-30 16:58:00 -0700

    Merge branch 'dev' into user_pages and taking out the hard coded statuses

--- app/status.py
+++ app/status.py
@@ -8,3 +8,11 @@ def get_status_id(name):
 def get_status(status_id):
 	cur = get_db().cursor()
 	return cur.execute('SELECT * FROM status WHERE status_id=?', str(status_id)).fetchone()['name']
+
+def get_all_statuses():
+	cur = get_db().cursor()
+	rows = cur.execute('SELECT * FROM status').fetchall()
+	statuses = []
+	for data in rows:
+		statuses.append(data['name'])
+	return statuses
