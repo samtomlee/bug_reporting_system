45171fcb5745cd78154161df9afbe76702a3009e
Thomas King <37191454+TKingVS@users.noreply.github.com>
2018-11-12 17:03:04 -0800

    Severity and status sorting is ordered by id instead of alphabetically

--- app/severity.py
+++ app/severity.py
@@ -1,18 +1,24 @@
 from .database import get_db
 
+class Severity:
+	def __init__(self, severity_id, name):
+		self.id = severity_id
+		self.name = name
+
 def get_severity_id(name):
 	print(name)
 	cur = get_db().cursor()
-	return cur.execute('SELECT * FROM severity WHERE name=?', (name,)).fetchone()['severity']
+	return cur.execute('SELECT * FROM severity WHERE name=?', (name,)).fetchone()['severity_id']
 
 def get_severity(severity_id):
 	cur = get_db().cursor()
-	return cur.execute('SELECT * FROM severity WHERE severity_id=?', str(severity_id)).fetchone()['name']
+	data = cur.execute('SELECT * FROM severity WHERE severity_id=?', str(severity_id)).fetchone()
+	return Severity(data['severity_id'], data['name'])
 
 def get_all_severities():
 	cur = get_db().cursor()
 	rows = cur.execute('SELECT * FROM severity').fetchall()
 	severities = []
 	for data in rows:
-		severities.append(data['name'])
+		severities.append(Severity(data['severity_id'], data['name']))
 	return severities
--- app/status.py
+++ app/status.py
@@ -1,5 +1,10 @@
 from .database import get_db
 
+class Status:
+	def __init__(self, status_id, name):
+		self.id = status_id
+		self.name = name
+
 def get_status_id(name):
 	print(name)
 	cur = get_db().cursor()
@@ -7,12 +12,13 @@ def get_status_id(name):
 
 def get_status(status_id):
 	cur = get_db().cursor()
-	return cur.execute('SELECT * FROM status WHERE status_id=?', str(status_id)).fetchone()['name']
+	data = cur.execute('SELECT * FROM status WHERE status_id=?', str(status_id)).fetchone()
+	return Status(data['status_id'], data['name'])
 
 def get_all_statuses():
 	cur = get_db().cursor()
 	rows = cur.execute('SELECT * FROM status').fetchall()
 	statuses = []
 	for data in rows:
-		statuses.append(data['name'])
+		statuses.append(Status(data['status_id'], data['name']))
 	return statuses
